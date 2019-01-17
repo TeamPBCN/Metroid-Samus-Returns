f937c4efd73b1aca6c4fc4091d3c32c24a19f968
littoma <littoma@163.com>
2019-01-17 14:33:21 +0800

    Add numbers to filters.

--- filter.py
+++ filter.py
@@ -6,12 +6,16 @@ from utils import read_messages
 def create_filter(path, msg_file, lbl_file):
     result = []
     messages = read_messages(msg_file)
-    labels = codecs.open(lbl_file, 'r', 'utf-16').read().split('\n')
+    labels = codecs.open(lbl_file, 'r', 'utf-16').read().replace('\r\n', '\n').split('\n')
 
     for m in messages:
         for label in labels:
             if label and re.match(label, m[0]):
                 result.append(m[1])
+    
+    nums = '%d'*10
+    nums = nums%tuple(range(10))
+    result.append(nums)
     
     codecs.open(path, 'w', 'utf-16').writelines(result)
 
